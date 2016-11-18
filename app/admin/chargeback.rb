72607ba68d621b982fbf0d92dc87805abf6c2742
Raghu Betina <raghu@firstdraft.com>
2016-11-18 15:48:51 +0000

    Whitelist columns in Chargeback for ActiveAdmin

--- app/admin/chargeback.rb
+++ app/admin/chargeback.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Chargeback do
 
+ permit_params :date_received, :date_transacted, :contact_name, :account_name, :salesperson_id, :credit_card_id, :amount, :io, :case_number, :reason_id, :date_due, :date_responded, :status_id, :notes, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
