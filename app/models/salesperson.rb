a7b7eae6d2b2177d91a86afe5c7ade3056dfde86
Raghu Betina <raghu@firstdraft.com>
2016-11-18 15:48:22 +0000

    Validate presence of salespeople.first_name

--- app/models/salesperson.rb
+++ app/models/salesperson.rb
@@ -5,6 +5,8 @@ class Salesperson < ApplicationRecord
 
   # Validations
 
+  validates :first_name, :presence => true
+
   validates :last_name, :uniqueness => { :scope => [:first_name] }
 
   validates :last_name, :presence => true
