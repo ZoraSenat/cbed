75f9315b1b0f8b6a62123a64056cd2df664aa7aa
Raghu Betina <raghu@firstdraft.com>
2016-11-18 15:48:55 +0000

    Whitelist columns in Salesperson for ActiveAdmin

--- app/admin/salesperson.rb
+++ app/admin/salesperson.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Salesperson do
 
+ permit_params :first_name, :last_name, :sugar_user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
