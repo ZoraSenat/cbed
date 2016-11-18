bb0edffd204aaa82225ed6285e4fda4115688948
Raghu Betina <raghu@firstdraft.com>
2016-11-18 15:48:22 +0000

    Validate uniqueness of salespeople.first_name

--- app/models/salesperson.rb
+++ app/models/salesperson.rb
@@ -5,6 +5,8 @@ class Salesperson < ApplicationRecord
 
   # Validations
 
+  validates :first_name, :uniqueness => { :scope => [:last_name] }
+
   validates :first_name, :presence => true
 
   validates :last_name, :uniqueness => { :scope => [:first_name] }
